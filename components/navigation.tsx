520e4e46c4c01a033b79b2f9b2ccfa0db5e81ee6
니꼬 <nico@nomadcoders.co>
2024-01-16 16:36:55 +0900

    1.6 Recap

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,9 +2,11 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
+  const [count, setCount] = useState(0);
   return (
     <nav>
       <ul>
@@ -14,6 +16,9 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About Us</Link>
           {path === "/about-us" ? "🔥" : ""}
+        </li>
+        <li>
+          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
