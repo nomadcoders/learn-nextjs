a7bfe23a2df09cacfa56c3e743d5e170af41de49
니꼬 <nico@nomadcoders.co>
2024-01-18 17:37:53 +0900

    2.3 Loading Components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,7 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(URL);
   const json = await response.json();
   return json;
