80a095ffc92d1be2234e53a7bbf4d2a8293f240c
니꼬 <nico@nomadcoders.co>
2024-01-18 17:07:05 +0900

    2.2 Server Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,10 +2,16 @@ export const metadata = {
   title: "Home",
 };
 
-export default function Page() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
